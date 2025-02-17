3dc1e91107686914c633215e5464124a0c496f50
paveltravnicek <80485198+paveltravnicek@users.noreply.github.com>
2025-02-17 07:50:12 +0100

    Update universal-functions.php

--- universal-functions.php
+++ universal-functions.php
@@ -139,9 +139,7 @@ add_action('pre_user_query', 'hide_paveltravnicek_from_users_list');
 
 function skryt_radek_akci_pro_chranene_pluginy() {
     const currentUser = wp_get_current_user();
-    const allowedUsers = ['paveltravnicek', 'lukashulka'];
-
-    if (allowedUsers.includes(currentUser.user_login)) {
+    if (currentUser.user_login === 'paveltravnicek') {
         return;
     }
 
@@ -172,17 +170,18 @@ add_action('admin_footer', 'skryt_radek_akci_pro_chranene_pluginy');
 
 function omez_pristup_branda_pro() {
     $current_user = wp_get_current_user();
-    if ($current_user->user_login === 'lukashulka') {
-        $current_screen = get_current_screen();
-        if (
-            strpos($current_screen->id, 'branda') !== false && 
-            !isset($_GET['tab']) || $_GET['tab'] !== 'emails' ||
-            !isset($_GET['subpage']) || $_GET['subpage'] !== 'logs'
-        ) {
-            wp_redirect(admin_url());
-            exit;
-        }
+    if ($current_user->user_login === 'paveltravnicek') {
+        return;
     }
+    
+    if ($current_user->user_login === 'lukashulka') {
+        return;
+    }
+    
+    add_action('admin_menu', function() {
+        remove_menu_page('branding');
+        remove_submenu_page('branding_group', 'email');
+    }, 999);
 }
 add_action('admin_init', 'omez_pristup_branda_pro');
 
